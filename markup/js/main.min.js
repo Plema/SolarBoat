3c002a13f9ff9b5f03a0bb5732ca5b12fc3cabd1
OdrinskayaMagarita <margarita.odrinskaya@gmail.com>
2020-04-27 16:13:34 +0300

    ready on the test

---
+++ markup/js/main.min.js
@@ -0,0 +1,17 @@
+// $(document).ready(function(){
+//   $('.menu-wrapper').on('click', function() {
+//     if($('nav').is('.open-nav')){
+//       $('.hamburger-menu').removeClass('animate');
+//       $('body').css('overflow-y','visible');
+//       $('.background-dark').removeClass('open-back');
+//       $('nav').removeClass('open-nav');
+//     }
+//     else{
+//       $('.menu-wrapper').addClass('close-menu')
+//       $('.hamburger-menu').addClass('animate');
+//       $('body').css('overflow-y','hidden');
+//       $('.background-dark').addClass('open-back');
+//       $('nav').addClass('open-nav'); 
+//     }
+//   });
+// });
